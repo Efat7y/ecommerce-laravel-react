b5eeff22f3d37df22753fe61f657969a5c907b01
Efat7y <eslamzain8897@gmail.com>
2026-09-22 21:04:33 +0300

    fix: allow flash_quantity and flash_sold to be mass assigned in FlashSaleProduct model

--- backend/app/Models/FlashSaleProduct.php
+++ backend/app/Models/FlashSaleProduct.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class FlashSaleProduct extends Model
 {
-    protected $fillable = ['flash_sale_id', 'product_id', 'discount_price'];
+    protected $fillable = [
+        'flash_sale_id', 
+        'product_id', 
+        'discount_price', 
+        'flash_quantity', 
+        'flash_sold'
+    ];
 }
