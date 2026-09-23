b1ffe27905bcb537a1e4b0883d49205567b2c028
Efat7y <eslamzain8897@gmail.com>
2026-09-23 18:29:36 +0300

    fix: remove array_filter that dropped false ssl verify flag

--- backend/config/database.php
+++ backend/config/database.php
@@ -59,10 +59,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ] : [],
         ],
 
         'mariadb' => [
@@ -80,10 +79,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
+            'options' => extension_loaded('pdo_mysql') ? [
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-                (PHP_VERSION_ID >= 80500 ? Mysql::ATTR_SSL_CA : PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            ] : [],
         ],
 
         'pgsql' => [
